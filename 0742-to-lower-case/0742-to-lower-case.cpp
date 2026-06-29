0d44af238f925cf2f85a5f03e6dabf1d701a4dba
vedant476 <156251188+vedant476@users.noreply.github.com>
2026-06-29 15:50:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (82.8%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for( int i = 0 ; i< s.size();i++){
+           if(s[i] <= 90  && s[i] >= 65 ) s[i] = s[i] - 'A'+'a'; 
+        }
+        return s;
+    }
+};
